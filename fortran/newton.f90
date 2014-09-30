e2f3984e8eff1c67ab75a41f6544c2070212895d
Francisco <chicosobral@gmail.com>
2014-09-29 22:32:35 -0300

    Development of asynchronous truncated Newton (with bugs)

--- fortran/newton.f90
+++ fortran/newton.f90
@@ -54,7 +54,7 @@ contains
     real(8), allocatable :: d(:),g(:),h(:,:)
     
     
-    allocate(d(n),g(n),h(n,n),STAT=status) ! Pegar o erro aqui!
+    allocate(d(n),g(n),h(n,n),STAT=status)
 
     if (status .ne. 0) then
        write(*,*) 'Memory problems'
@@ -123,5 +123,209 @@ contains
 9010 FORMAT('Inner GS took',1X,I0.5,1X,'iterations.',1X,'RESIDUAL=',1X,E10.4)
 
   end subroutine tnmin_serial
+
+  ! ---------------------------------------------------------------- !
+  ! ---------------------------------------------------------------- !
+
+  subroutine tnmin_async(n,x,d,func,grad,hess,epslin,epsopt,maxit,flag)
+
+    use gs ! function 'ceil'
+
+    implicit none
+
+    ! SCALAR ARGUMENTS
+    integer :: n,maxit,flag
+    real(8) :: epslin,epsopt
+
+    ! ARRAY ARGUMENTS
+    real(8) :: x(n),d(n)
+
+    ! FUNCTION ARGUMENTS
+    INTERFACE
+       subroutine func(n,x,f)
+         integer, intent( in) :: n
+         real(8), intent(out) :: f
+         real(8), intent( in) :: x(n)
+       end subroutine func
+    end INTERFACE
+
+    INTERFACE
+       subroutine grad(n,x,g)
+         integer, intent( in) :: n
+         real(8), intent(out) :: g(n)
+         real(8), intent( in) :: x(n)
+       end subroutine grad
+    end INTERFACE
+    
+    INTERFACE
+       subroutine hess(n,x,h)
+         integer, intent( in) :: n
+         real(8), intent(out) :: h(n,n)
+         real(8), intent( in) :: x(n)
+       end subroutine hess
+    end INTERFACE
+
+    ! LOCAL SCALARS
+    integer :: np,tn,start,end
+
+    ! FUNCTIONS
+    integer omp_get_num_threads,omp_get_thread_num
+
+    !$OMP PARALLEL PRIVATE(np,tn,start,end)
+
+    np = omp_get_num_threads()
+
+    tn = omp_get_thread_num()
+
+    start = 1 + ceil(tn * n / (1.0D0 * np))
+
+    if (tn .eq. np - 1) then
+       end = n
+    else
+       end = ceil((tn + 1) * n / (1.0D0 * np))
+    end if
+
+    call tnmin_partial(n,x,d,func,grad,hess,start,end,epslin,epsopt,&
+         maxit,flag)
+
+    !$OMP END PARALLEL
+
+  end subroutine tnmin_async
+  
+
+  ! ---------------------------------------------------------------- !
+  ! ---------------------------------------------------------------- !
+
+  subroutine tnmin_partial(n,x,d,func,grad,hess,start,end,epslin,epsopt,&
+       maxit,flag)
+    
+    use gs
+
+    implicit none
+
+    ! SCALAR ARGUMENTS
+    integer :: start,n,maxit,flag,end
+    real(8) :: epslin,epsopt
+
+    ! ARRAY ARGUMENTS
+    real(8) :: x(n),d(n)
+
+    ! FUNCTION ARGUMENTS
+    INTERFACE
+       subroutine func(n,x,f)
+         integer, intent( in) :: n
+         real(8), intent(out) :: f
+         real(8), intent( in) :: x(n)
+       end subroutine func
+    end INTERFACE
+
+    INTERFACE
+       subroutine grad(n,x,g)
+         integer, intent( in) :: n
+         real(8), intent(out) :: g(n)
+         real(8), intent( in) :: x(n)
+       end subroutine grad
+    end INTERFACE
+    
+    INTERFACE
+       subroutine hess(n,x,h)
+         integer, intent( in) :: n
+         real(8), intent(out) :: h(n,n)
+         real(8), intent( in) :: x(n)
+       end subroutine hess
+    end INTERFACE
+
+    ! LOCAL SCALARS
+    integer :: i,iter,initer,inmaxit,status,proc
+    real(8) :: f,gnorm,inepsilon
+
+    ! LOCAL ARRAYS
+    real(8), allocatable :: g(:),h(:,:)
+    real(8) :: r(start:end)
+
+    ! FUNCTIONS
+    integer omp_get_thread_num
+
+    allocate(g(n),h(n,n),STAT=status)
+
+    if (status .ne. 0) then
+       write(*,*) 'Memory problems'
+       flag = -1
+
+       stop
+    end if
+
+    ! Initial calculations
+
+    call func(n,x,f)
+    call grad(n,x,g)
+
+    gnorm = 0.0D0
+    do i = 1,n
+       g(i) = - g(i)
+       gnorm = max(gnorm,abs(g(i)))
+       d(i) = 0.0D0
+    end do
+
+    iter = 1
+
+    inepsilon = 5.0D-1;
+
+    inmaxit = 10
+
+    proc = omp_get_thread_num()
+
+    ! Main loop
+
+    do while (gnorm .gt. epsopt .and. iter .le. maxit) 
+
+       write(*,FMT=8000) proc,iter,f,gnorm
+
+       ! Calculates the step
+
+       call hess(n,x,h)
+
+       inepsilon = min(gnorm,sqrt(inepsilon))
+
+       ! Teste do Emerson: apenas 1 iteracao
+       !call updatex(start,end,n,h,g,d)
+       ! Meu teste: criterio decrescente
+       do i = start,end
+          r(i) = 1.0D+20
+       end do
+       call partialgs(n,h,g,d,r,start,end,initer,inepsilon,inmaxit)
+
+       write(*,FMT=8010) proc,initer,maxval(r)
+
+       ! Updates the point
+
+       do i = start,end
+          x(i) = x(i) + d(i)
+       end do
+
+       call func(n,x,f)
+       call grad(n,x,g)
+
+       gnorm = 0.0D0
+       do i = 1,n
+          g(i) = - g(i)
+          gnorm = max(gnorm,abs(g(i)))
+       end do
+
+       iter = iter + 1
+
+    end do
+
+    write(*,FMT=8000) proc,iter,f,gnorm
+
+    deallocate(g,h)
+
+8000 FORMAT('Proc',1X,I2,1X,'Iter:',1X,I0.10,1X,'F=',1X,E10.4,1X,&
+          'NORMG=',1X,E10.4)
+8010 FORMAT('Proc',1X,I2,1X,'Inner GS took',1X,I0.5,1X,&
+          'iterations.',1X,'RESIDUAL=',1X,E10.4)
+
+  end subroutine tnmin_partial
+  
 
 end module newton
--- fortran/test_newton.f90
+++ fortran/test_newton.f90
@@ -5,25 +5,26 @@ program test_newton
   implicit none
 
   integer :: n,maxit,flag,i
-  real(8),allocatable :: x(:)
+  real(8),allocatable :: x(:),d(:)
   real(8) :: epslin,epsopt
 
   epslin = 1.0D-8
   epsopt = 1.0D-8
-  maxit = 1000
+  maxit = 100000000
 
   write(*,*) 'Digite n'
   read(*,*) n
 
-  allocate(x(n))
+  allocate(x(n),d(n))
 
   do i = 1,n
-     x(i) = 5.0D0
+     x(i) = -1.0D0
+     d(i) =  0.0D0
   end do
 
-  call tnmin_serial(n,x,func,grad,hess,epslin,epsopt,maxit,flag)
+  call tnmin_async(n,x,d,func,grad,hess,epslin,epsopt,maxit,flag)
 
-  deallocate(x)
+  deallocate(x,d)
 
 contains
 
@@ -35,8 +36,9 @@ contains
     integer :: i
 
     f = 0.0D0
-    do i = 1,n
-       f = f + x(i) ** 2.0D0
+    do i = 1,n - 1
+       f = f + (1.0D0 - x(i)) ** 2.0D0 &
+             + 100.0D0 * (x(i + 1) - x(i) ** 2.0D0) ** 2.0D0
     end do
 
   end subroutine func
@@ -48,8 +50,13 @@ contains
 
     integer :: i
 
-    do i = 1,n
-       g(i) = 2.0D0 * x(i)
+    do i = 1,n - 1
+       g(i) = - 2.0D0 * (1.0D0 - x(i)) &
+              - 4.0D+2 * (x(i + 1) - x(i) ** 2.0D0) * x(i)
+    end do
+    g(n) = 0.0D0
+    do i = 2,n
+       g(i) = g(i) + 2.0D+2 * (x(i) - x(i - 1) ** 2.0D0)
     end do
 
   end subroutine grad
@@ -61,13 +68,17 @@ contains
 
     integer :: i,j
 
-    do j = 1,n
-       do i = 1,j-1
+    do j = 2,n - 1
+       h(j,j + 1) = - 4.0D+2 * x(j)
+       h(j + 1,j) = h(j,j + 1)
+       do i = j + 2,n
           h(i,j) = 0.0D0
           h(j,i) = 0.0D0
        end do
-       h(j,j) = 2.0D0
+       h(j,j) = 2.02D+2 + 1.2D+3 * x(j) ** 2.0D0 - 4.0D+2 * x(j + 1)
     end do
+    h(1,1) = 2.0D0 + 1.2D+3 * x(1) ** 2.0D0 - 4.0D+2 * x(2)
+    h(n,n) = 2.0D+2
 
   end subroutine hess
 
